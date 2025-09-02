b16c8394c8eabf566f8d1d78e291c2c518cdd266
David A <oludavid@protonmail.com>
2025-09-02 11:20:03 -0400

    Updated blog post: AI-driven attacks

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,6 +1,6 @@
 
-import __ASTRO_IMAGE_IMPORT_kJ1bp from "../../assets/test.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fthoughts-on-emerging-AI-threat-actors.md";
 import __ASTRO_IMAGE_IMPORT_IJBKk from "../../assets/apt-detection-lab.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fai-powered-apt-detection-lab.md";
+import __ASTRO_IMAGE_IMPORT_kJ1bp from "../../assets/test.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fthoughts-on-emerging-AI-threat-actors.md";
 import __ASTRO_IMAGE_IMPORT_ZMlUqm from "../../assets/blog-splunk-vs-elastic.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fsplunk-vs-elastic.md";
-export default new Map([["../../assets/test.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fthoughts-on-emerging-AI-threat-actors.md", __ASTRO_IMAGE_IMPORT_kJ1bp], ["../../assets/apt-detection-lab.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fai-powered-apt-detection-lab.md", __ASTRO_IMAGE_IMPORT_IJBKk], ["../../assets/blog-splunk-vs-elastic.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fsplunk-vs-elastic.md", __ASTRO_IMAGE_IMPORT_ZMlUqm]]);
+export default new Map([["../../assets/apt-detection-lab.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fai-powered-apt-detection-lab.md", __ASTRO_IMAGE_IMPORT_IJBKk], ["../../assets/test.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fthoughts-on-emerging-AI-threat-actors.md", __ASTRO_IMAGE_IMPORT_kJ1bp], ["../../assets/blog-splunk-vs-elastic.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fsplunk-vs-elastic.md", __ASTRO_IMAGE_IMPORT_ZMlUqm]]);
 		
